abd989f545a2f83f273ca2bb1fa38f9b55abcbe2
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-01-29 12:51:19 +0530

    Add files via upload

---
+++ JavaCodes/EmpSalarySlip/CommonUtils.java
@@ -0,0 +1,9 @@
+public class CommonUtils {
+	
+	// Formatting of Name
+	
+	// Formatting of Salary in different currency
+	
+	// Formatting of Date
+
+}
---
+++ JavaCodes/EmpSalarySlip/EmployeeSalarySlip.java
@@ -0,0 +1,7 @@
+public class EmployeeSalarySlip {
+//	Generate Salary Slip
+//	Where
+//	Earnings = Basic Salary + HRA (20%) + TA(10%) + DA(10%) + MA(15%)
+//	Deductions = TDS (15%) + PF(10%)
+//	Calculate Total Salary based on earning and deductions
+}
