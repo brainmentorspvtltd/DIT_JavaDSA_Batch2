73d9c4ea91823aef187158be89a26354693767b0
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-02-05 12:10:52 +0530

    Add files via upload

--- JavaCodes/EmpSalarySlip/CommonUtils.java
+++ JavaCodes/EmpSalarySlip/CommonUtils.java
@@ -1,9 +1,43 @@
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class CommonUtils {
 	
+	ResourceBundle rb = ResourceBundle.getBundle("constants");
+	Locale locale = new Locale(rb.getString("localeFormat"), rb.getString("localeCountry"));
+	
 	// Formatting of Name
+	public String getProperName(String name) {
+		/*
+		 * name = "john CeNa"; 
+		 * nameArr = ["john", "CeNa"];
+		 */
+		String nameArr[] = name.split(" ");
+		String properName = "";
+		for (String n : nameArr) {
+			properName += String.valueOf(n.charAt(0)).toUpperCase() + 
+					n.substring(1).toLowerCase() + " ";
+		}
+		return properName;
+		
+	}
 	
 	// Formatting of Salary in different currency
+	public String formatSalary(double salary) {	
+		NumberFormat nf = NumberFormat.getCurrencyInstance(locale);
+		String formatSal = nf.format(salary);
+		return formatSal;
+	}
 	
 	// Formatting of Date
+	public String formatDate() {
+		Date date = new Date();
+		DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+		String formattedDate = df.format(date);
+		return formattedDate;
+	}
 
 }
--- JavaCodes/EmpSalarySlip/EmployeeTest.java
+++ JavaCodes/EmpSalarySlip/EmployeeTest.java
@@ -2,7 +2,8 @@ public class EmployeeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EmployeeSalarySlip obj = new EmployeeSalarySlip();
+		EmployeeSalarySlip obj = new EmployeeSalarySlip(101, "surYa KumaR YadAv", 50000);
+		System.out.println(obj.showEmp());
 	}
 
 }
