eab5512e709486b7e65d926373649520b37eeeba
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-07-14 15:32:00 +0530

    Add files via upload

--- DSA/Tree/BinaryTreeOperations.java
+++ DSA/Tree/BinaryTreeOperations.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BinaryTreeOperations {
     String message = "root";
@@ -50,6 +52,131 @@ public class BinaryTreeOperations {
         System.out.println(root.data);
     }
 
+    void preOrderIterative(BinaryTree<Integer> root) {
+        // Root, Left, Right
+        /*
+         * 1. Create an Empty Stack
+         * 2. Push root node in stack
+         * 3. While Stack is not empty
+         *      - pop an item from stack & print it
+         *      - push right child of popped item in stack
+         *      - push left child of popped item in stack
+         */
+        if(root == null) {
+            return;
+        }
+        Stack<BinaryTree<Integer>> st = new Stack<>();
+        BinaryTree<Integer> current = root;
+        while(!st.isEmpty() || current != null) {
+            if(!st.isEmpty() && current == null) {
+                current = st.pop();
+            }
+            System.out.println(current.data);
+            if(current.right != null) {
+                st.push(current.right);
+            }
+            current = current.left;
+        }
+    }
+
+    ArrayList<Integer> postOrderIterative(BinaryTree<Integer> root) {
+        // Left, Right, Root
+        /*
+         * 1. If right child exist of 1
+         *      - push 3 to stack, push 1 to stack and move to left child
+         *      Stack : 3,1
+         * 2. If right child of 2 exists
+         *      - push 5 to stack, push 2 to stack and move to left child
+         *      Stack : 3,1,5,2
+         * 3. Now right child do not exists for 4
+         *      - push 4 to stack and move to left
+         *      Stack : 3,1,5,2,4
+         * 4. Current Node is null
+         *      - Pop 4 from Stack. Right child of 4 doesn't exists
+         *      - Print 4 and set current node to null
+         *      Stack : 3,1,5,2
+         * 5. Current Node is null
+         *      - Pop 2 from stack
+         *          - check if stack peek and popped element right node is same
+         *      - Pop 5 from stack and push 2 back into stack
+         *      - Move current node to right child of 2 i.e.,5
+         *      Stack : 3,1,2
+         * 6. Right child of 5 doesn't exists, push 5 to stack, move to left child
+         *      Stack : 3,1,2,5
+         * 7. Current Node is Null. Pop 5 from stack
+         *      - Print 5 and set current node to null
+         *      Stack : 3,1,2
+         * 8. Current Node is Null. Pop 2 from stack
+         *      - check if stack peek and popped element right node is same
+         *      - print 2 and set current node to null
+         *      Stack : 3,1
+         * 9. Current Node is null
+         *      - Pop 1 from stack
+         *          - check if stack peek and popped element right node is same
+         *      - Pop 3 from stack and push 1 back into stack
+         *      - Move current node to right child of 1 i.e., 3
+         *      Stack : 1
+         * 10. Right child of 3 exists
+         *      - push 7 to stack and push 3 to stack and move to left child
+         *      Stack : 1, 7, 3
+         * 11. Right child of 6 doesn't exists
+         *      - push 6 to stack and move to left child
+         *         Stack : 1,7,3,6
+         * 12. Current Node is null
+         *      - pop 6 from stack, right child of 6 doesn't exists
+         *      - print 6, set current node as null
+         *      Stack : 1,7,3
+         * 13. Current Node is null
+         *      - pop 3 from stack and 
+         *        check stack peek and current node right element is same
+         *      - pop 7 from stack and push 3 back to stack
+         *      Stack : 1,3
+         */
+
+        Stack<BinaryTree<Integer>> st = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return list;
+        }
+
+        st.push(root);
+        BinaryTree<Integer> prev = null;
+        while(!st.isEmpty()) {
+            BinaryTree<Integer> current = st.peek();
+            if(prev == null || prev.left == current || prev.right == current) {
+                if(current.left != null) {
+                    st.push(current.left);
+                }
+                else if(current.right != null) {
+                    st.push(current.right);
+                }
+                else {
+                    st.pop();
+                    list.add(current.data);
+                }
+            }
+
+            else if(current.left == prev) {
+                if(current.right != null) {
+                    st.push(current.right);
+                }
+                else {
+                    st.pop();
+                    list.add(current.data);
+                }
+            }
+
+            else if(current.right == prev) {
+                st.pop();
+                list.add(current.data);
+            }
+            prev = current;
+        }
+
+        return list;
+
+    }
+
     void print(BinaryTree<Integer> root) {
         if(root == null) {
             return;
@@ -71,5 +198,7 @@ public class BinaryTreeOperations {
         BinaryTreeOperations tree = new BinaryTreeOperations();
         BinaryTree<Integer> root = tree.insert();
         tree.print(root);
+
+        tree.postOrderIterative(root);
     }    
 }
